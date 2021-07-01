ae264ffc40a721f139e7c424b83e16342b35b182
peter olu <peter.o.olu@gmail.com>
2021-07-01 17:16:10 +0100

    styled top section of the page

--- pages/community.vue
+++ pages/community.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
       <div class="container mt-5 p-0">
-          <div class="col-12">
+          <div class="">
               <h1>Support Communities</h1>
           </div>
           <div class="row">
               <div class="col-10">
                   <div class="d-flex w-100 align-items-center">
-                      <div class="total-topics btn btn-primary">290 topics</div>
+                      <div class="total-topics btn btn-primary d-flex justify-content-center align-items-center">290 topics</div>
                         <div class="topic-tabs ml-auto">
                             <nav class="nav nav-pills flex-column flex-sm-row">
                                 <a class="flex-sm-fill text-sm-center mx-3 nav-link active" href="#">TRENDING</a>
-                                <a class="flex-sm-fill text-sm-center mx-3 nav-link" href="#">A - Z</a>
-                                <a class="flex-sm-fill text-sm-center mx-3 nav-link" href="#">BY TOPIC</a>
-                                <a class="flex-sm-fill text-sm-center mx-3 nav-link" href="#">OTHERS</a>
+                                <a class="flex-sm-fill text-sm-center mx-3 nav-link text-muted" href="#">A - Z</a>
+                                <a class="flex-sm-fill text-sm-center mx-3 nav-link text-muted" href="#">BY TOPIC</a>
+                                <a class="flex-sm-fill text-sm-center mx-3 nav-link text-muted" href="#">OTHERS</a>
                             </nav>
                         </div>
                   </div>
@@ -29,7 +29,7 @@
                     
                   <div class="rounded w-100 bg-secondary mt-3 create-discussion p-4">
                       <div class="d-flex flex-column align-items-center justif flex-md-row">
-                          <p>Can’t find what you’re looking for?</p>
+                          <p class="m-0">Can’t find what you’re looking for?</p>
                           <div class="btn btn-primary ml-md-3">Create a new discussion</div>
                       </div>
                   </div>
@@ -87,6 +87,24 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.total-topics.btn{
+    border-radius: 90px;
+    width: 102px;
+    height:26px;
+}
+.list-group-item{
+    padding:0;
+    border: 0;
+    color: rgba(59, 140, 215, 1);
+}
+.nav.nav-pills a{
+    background: transparent;
+    &.active{
+        border-bottom: 1px solid #119859;
+        border-radius: 0;
+        color: #119859
+    }
+}
 
 </style>
