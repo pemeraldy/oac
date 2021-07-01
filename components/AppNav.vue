1659f0c9f1227e77d8120fb8116237c40acc7958
peter olu <peter.o.olu@gmail.com>
2021-07-01 13:39:08 +0100

    route to sign in

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -20,7 +20,7 @@
         <a class="nav-link text-white" href="#">About Us</a>
       </li>
       <li class="nav-item mx-3">
-        <button class="btn nav-btn btn-outline-success my-2 my-sm-0" type="submit">Sign in</button>
+        <nuxt-link to="/auth/sign-in" tag="button" class="btn nav-btn btn-outline-success my-2 my-sm-0" type="submit">Sign in</nuxt-link>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0 position-relative">
