8642f5b3adbecc251d6062aa91702c237c2b5025
peter olu <peter.o.olu@gmail.com>
2021-07-01 15:27:06 +0100

    password request form

---
+++ components/RequestPwResetForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-100">
+    <form>        
+        <div class="form-group required">
+            <label for="userpassword">Email*</label>
+            <input type="email" class="form-control" id="userpassword">
+        </div>
+        
+        <div class="my-5">
+            <button type="submit" class="btn btn-block btn-primary">Request password reset</button>
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Request-pw-form',
+}
+</script>
+
+<style>
+
+</style>
