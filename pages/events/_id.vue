0b83a17d172a1b4a7685c5007baa465d7a87f2f5
peter olu <peter.o.olu@gmail.com>
2021-07-02 10:57:59 +0100

    event detail page

--- pages/events/_id.vue
+++ pages/events/_id.vue
@@ -29,7 +29,8 @@
             abuse, how we should stay away from antibiotics if our doctors
             haven’t prescripted them and how we should use antibiotics in
             general. We believe that we had an impact on quite a lot of people
-            whom we contacted. Contrary to popular belief, Lorem Ipsum is not
+            whom we contacted.</p> 
+            <p> Contrary to popular belief, Lorem Ipsum is not
             simply random text. It has roots in a piece of classical Latin
             literature from 45 BC, making it over 2000 years old. Richard
             McClintock, a Latin professor at Hampden-Sydney College in Virginia,
@@ -40,8 +41,9 @@
             Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
             BC. This book is a treatise on the theory of ethics, very popular
             during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum
-            dolor sit amet..", comes from a line in section 1.10.32. The
-            standard chunk of Lorem Ipsum used since the 1500s is reproduced
+            dolor sit amet..", comes from a line in section 1.10.32.</p>
+            <p>
+            The standard chunk of Lorem Ipsum used since the 1500s is reproduced
             below for those interested. Sections 1.10.32 and 1.10.33 from "de
             Finibus Bonorum et Malorum" by Cicero are also reproduced in their
             exact original form, accompanied by English versions from the 1914
